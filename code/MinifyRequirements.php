b165269f65880ebcf696df7efab58b9de08e03ec
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:59:45 +1300

    Converted to PSR-2

--- code/MinifyRequirements.php
+++ code/MinifyRequirements.php
@@ -1,39 +1,38 @@
 <?php
 
-class Minify_Requirements_Backend extends Requirements_Backend {
+class Minify_Requirements_Backend extends Requirements_Backend
+{
 
-	/**
-	 * @var bool
-	 */
-	private static $rewrite_uris = true;
+    /**
+     * @var bool
+     */
+    private static $rewrite_uris = true;
 
-	/**
-	 * @param string $filename
-	 * @param string $content
-	 * @return string
-	 */
-	protected function minifyFile($filename, $content) {
+    /**
+     * @param string $filename
+     * @param string $content
+     * @return string
+     */
+    protected function minifyFile($filename, $content)
+    {
 
-		// if we have a javascript file and jsmin is enabled, minify the content
-		$isJS = stripos($filename, '.js');
-		if($isJS && $this->combine_js_with_jsmin) {
+        // if we have a javascript file and jsmin is enabled, minify the content
+        $isJS = stripos($filename, '.js');
+        if ($isJS && $this->combine_js_with_jsmin) {
+            increase_time_limit_to();
+            $content = JSMin::minify($content);
+        } elseif (stripos($filename, '.css')) {
+            increase_time_limit_to();
+            $minifyCSSConfig = array();
 
-			increase_time_limit_to();
-			$content = JSMin::minify($content);
-		} else if (stripos($filename, '.css')) {
+            if (Config::inst()->get('Minify_Requirements_Backend', 'rewrite_uris')) {
+                $minifyCSSConfig['currentDir'] = Director::baseFolder() . '/' . dirname($filename);
+            }
 
-			increase_time_limit_to();
-			$minifyCSSConfig = array();
+            $content = Minify_CSS::minify($content, $minifyCSSConfig);
+        }
 
-			if (Config::inst()->get('Minify_Requirements_Backend', 'rewrite_uris')) {
-				$minifyCSSConfig['currentDir'] = Director::baseFolder() . '/' . dirname($filename);
-			}
-
-			$content = Minify_CSS::minify($content, $minifyCSSConfig);
-		}
-
-		$content .= ($isJS ? ';' : '') . "\n";
-		return $content;
-	}
-
+        $content .= ($isJS ? ';' : '') . "\n";
+        return $content;
+    }
 }
